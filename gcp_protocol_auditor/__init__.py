80c5e0e5885844779abe5dfde67523b9ceef2e8d
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-26 00:02:38 +0530

    Initial Commit

---
+++ gcp_protocol_auditor/__init__.py
@@ -0,0 +1,6 @@
+"""
+ICH-GCP Protocol Deviation & Good Clinical Practice Auditor Agent
+Domain: Clinical Trials
+Standard: ICH E6(R2) Good Clinical Practice
+"""
+__version__ = "2.0.0-FRONTIER"
---
+++ gcp_protocol_auditor_app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from gcp_protocol_auditor.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main())
